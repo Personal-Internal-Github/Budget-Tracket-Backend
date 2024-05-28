0a35ebd6d4cc072d619c27da98066e5d82ec1fe1
Aiman Irfan <aimanirfan1017@gmail.com>
2024-05-28 16:56:10 +0800

    Add Cors package

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/test', (req, res) => {
   res.send('Connected to backend server!')
