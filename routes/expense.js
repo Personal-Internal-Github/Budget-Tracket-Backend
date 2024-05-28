815f093e819275e6672b0afa62d2981ff968926c
Aiman Irfan <aimanirfan1017@gmail.com>
2024-05-28 16:00:13 +0800

    Fix spelling mistake

--- routes/expense.js
+++ routes/expense.js
@@ -5,42 +5,42 @@ const Joi = require("joi");
 
 const db = require('../configurations/db');
 
-router.get('/getExpanse', (req, res) => {
-  db.query('SELECT * FROM expanse', (dbError, dbResult) => {
+router.get('/getExpense', (req, res) => {
+  db.query('SELECT * FROM expense', (dbError, dbResult) => {
     if (dbError) return res.status(501).send({response: 'ERROR', message: dbError});
 
     res.send({response: 'Success', message: dbResult});
   })
 });
 
-router.post('/addExpanse', (req, res) => {
+router.post('/addExpense', (req, res) => {
   const joiSchema = Joi.object({
-    expanseAmount: Joi.number(),
-    expanseDescription: Joi.string()
+    expenseAmount: Joi.number(),
+    expenseDescription: Joi.string()
   })
 
   const requestData = {
-    expanseAmount: req.body.expanseAmount,
-    expanseDescription: req.body.expanseDescription
+    expenseAmount: req.body.expenseAmount,
+    expenseDescription: req.body.expenseDescription
   }
 
   const joiValidation = joiSchema.validate(requestData);
 
   if(joiValidation.error) return res.send({response: 'Failed', message: joiValidation.error.message});
 
-  db.query('INSERT INTO expanse(expanse_amount, expanse_description) VALUES(?,?)', [requestData.expanseAmount, requestData.expanseDescription], (dbError, dbResult) => {
+  db.query('INSERT INTO expense(expense_amount, expense_description) VALUES(?,?)', [requestData.expenseAmount, requestData.expenseDescription], (dbError, dbResult) => {
     if(dbError) return res.status(501).send({response: 'ERROR', message: dbError});
 
     res.send({response: 'Success', message: 'Successfully add expanse!'});
   })
 });
 
-router.delete('/removeExpanse', (req, res) => {
+router.delete('/removeExpense', (req, res) => {
   const requestData = {
-    expanseId: req.body.id
+    expenseId: req.body.id
   }
 
-  db.query('DELETE FROM expanse where id = ?', requestData.expanseId, (dbError, dbResult) => {
+  db.query('DELETE FROM expense where id = ?', requestData.expenseId, (dbError, dbResult) => {
     if(dbError) return res.status(501).send({response: 'ERROR', message: dbError});
 
     res.send({response: 'Success', message: 'Successfully removed expanse record!'});
